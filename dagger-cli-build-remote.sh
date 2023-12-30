ca8f60d2e28f2e359bf5165af9c5c93721945c59
Victor Payno <vpayno@users.noreply.github.com>
2023-12-29 22:15:15 -0800

    cli: update dagger-cli-build-remote.sh

--- dagger-cli-build-remote.sh
+++ dagger-cli-build-remote.sh
@@ -1,45 +1,52 @@
 #!/bin/bash
 
 # get Go examples source code repository
-source=$(dagger query <<EOF | jq -r .git.branch.tree.id
-{
-  git(url:"https://go.googlesource.com/example") {
-    branch(name:"master") {
-      tree {
-        id
-      }
-    }
-  }
-}
-EOF
+source=$(
+	dagger query <<-EOF | jq -r .git.branch.tree.id
+		{
+		  git(url:"https://go.googlesource.com/example") {
+		    branch(name:"master") {
+		      tree {
+		        id
+		      }
+		    }
+		  }
+		}
+	EOF
 )
 
 # mount source code repository in golang container
 # build Go binary
 # export binary from container to host filesystem
-build=$(dagger query <<EOF | jq -r .container.from.withDirectory.withWorkdir.withExec.file.export
-{
-  container {
-    from(address:"golang:latest") {
-      withDirectory(path:"/src", directory:"$source") {
-        withWorkdir(path:"/src/hello") {
-          withExec(args:["go", "build", "-o", "dagger-builds-hello", "."]) {
-            file(path:"./dagger-builds-hello") {
-              export(path:"./dagger-builds-hello")
-            }
-          }
-        }
-      }
-    }
-  }
-}
-EOF
+result=$(
+	dagger query <<-EOF | jq -r .container.from.withDirectory.withWorkdir.withExec.withExec.withExec.file.export
+		{
+		  container {
+		    from(address:"golang:latest") {
+		      withDirectory(path:"/src", directory:"$source") {
+		        withWorkdir(path:"/src/hello") {
+		          withExec(args:["pwd"]) {
+		            withExec(args:["go", "clean", "./..."]) {
+		              withExec(args:["go", "build", "-o", "dagger-builds-hello", "."]) {
+		                file(path:"./dagger-builds-hello") {
+		                  export(path:"./dagger-builds-hello")
+		                }
+		              }
+		            }
+		          }
+		        }
+		      }
+		    }
+		  }
+		}
+	EOF
 )
 
+printf "Result: [%s]\n" "${result}"
+
 # check build result and display message
-if [ "$build" == "true" ]
-then
-    echo "Build successful"
+if [[ ${result} == "true" ]]; then
+	echo "Build successful"
 else
-    echo "Build unsuccessful"
+	echo "Build unsuccessful"
 fi
